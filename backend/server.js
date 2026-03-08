71cdc2f5b510fe21110972e5f55b4006a08a7aad
Lissa <lissananda4@gmail.com>
2026-03-08 20:18:13 +0530

    Full stack version with leaderboard backend

---
+++ backend/server.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const cors = require("cors");
+const fs = require("fs");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const file = "scores.json";
+
+app.get("/leaderboard", (req,res)=>{
+    const data = JSON.parse(fs.readFileSync(file));
+    res.json(data);
+});
+
+app.post("/score", (req,res)=>{
+
+const scores = JSON.parse(fs.readFileSync(file));
+
+const newScore = {
+name: req.body.name,
+level: req.body.level,
+time: req.body.time
+};
+
+scores.push(newScore);
+
+// sort fastest time first
+scores.sort((a,b)=>a.time-b.time);
+
+// keep only top 10
+const top10 = scores.slice(0,10);
+
+fs.writeFileSync(file, JSON.stringify(top10,null,2));
+
+res.json({message:"Score saved"});
+
+});
+
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
